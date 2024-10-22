d0bd261797fcf02373b814f2f9d5518d4095b960
Sikandar Ali <ali050786@gmail.com>
2024-10-22 12:36:00 +0530

    added sidebar with state

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,13 +1,35 @@
-// src/features/editor/components/editor.tsx
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { fabric } from 'fabric';
+
 import { useEditor } from '../hooks/use-editor';
-import { Navbar } from './navbar'
+import { Navbar } from './navbar';
+import { Sidebar } from './sidebar';
+import { ActiveTool } from '../active-types';
 
 export const Editor = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
+  const [activeTool, setActiveTool] = useState<ActiveTool>("select");
   
   const { init } = useEditor();
+
+  const onChangeActiveTool = useCallback((tool: ActiveTool) => {
+    // If clicking same tool, reset to select
+    if (tool === activeTool) {
+      return setActiveTool("select");
+    }
+
+    // Handle special case for draw tool
+    if (tool === "draw") {
+      // TODO: Enable draw mode
+    }
+
+    if (activeTool === "draw") {
+      // TODO: Disable draw mode
+    }
+
+    setActiveTool(tool);
+  }, [activeTool]);
 
   useEffect(() => {
     if (!canvasRef.current || !containerRef.current) return;
@@ -24,15 +46,24 @@ export const Editor = () => {
 
   return (
     <div className="h-full flex flex-col">
-      <Navbar />
-      <div 
-        ref={containerRef} 
-        className="flex-1 bg-muted relative overflow-hidden"
-      >
-        <canvas 
-          ref={canvasRef}
-          className="absolute inset-0 w-full h-full"
+      <Navbar 
+        activeTool={activeTool} 
+        onChangeActiveTool={onChangeActiveTool} 
+      />
+      <div className="flex-1 flex">
+        <Sidebar 
+          activeTool={activeTool}
+          onChangeActiveTool={onChangeActiveTool}
         />
+        <div 
+          ref={containerRef} 
+          className="flex-1 bg-muted relative overflow-hidden"
+        >
+          <canvas 
+            ref={canvasRef}
+            className="absolute inset-0 w-full h-full"
+          />
+        </div>
       </div>
     </div>
   );
--- src/features/editor/components/navbar.tsx
+++ src/features/editor/components/navbar.tsx
@@ -1,3 +1,5 @@
+import { MousePointerClick, ChevronDown, File } from "lucide-react";
+import { ActiveTool } from "../active-types";
 import { Button } from "../../../components/ui/button";
 import { Separator } from "../../../components/ui/separator";
 import { 
@@ -6,15 +8,26 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "../../../components/ui/dropdown-menu";
-import { MousePointerClick, ChevronDown, File } from "lucide-react";
 import { Hint } from "../../../components/hint";
+import { cn } from "../../../lib/utils";
 
-export const Navbar = () => {
+interface NavbarProps {
+  activeTool: ActiveTool;
+  onChangeActiveTool: (tool: ActiveTool) => void;
+}
+
+export const Navbar = ({
+  activeTool,
+  onChangeActiveTool,
+}: NavbarProps) => {
   return (
     <nav className="w-full flex items-center p-4 h-[68px]">
-        <div className="px-4">
-        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#F06225" d="M20 0c11.046 0 20 8.954 20 20v14a6 6 0 0 1-6 6H21v-8.774c0-2.002.122-4.076 1.172-5.78a10 10 0 0 1 6.904-4.627l.383-.062a.8.8 0 0 0 0-1.514l-.383-.062a10 10 0 0 1-8.257-8.257l-.062-.383a.8.8 0 0 0-1.514 0l-.062.383a9.999 9.999 0 0 1-4.627 6.904C12.85 18.878 10.776 19 8.774 19H.024C.547 8.419 9.29 0 20 0Z"></path><path fill="#F06225" d="M0 21h8.774c2.002 0 4.076.122 5.78 1.172a10.02 10.02 0 0 1 3.274 3.274C18.878 27.15 19 29.224 19 31.226V40H6a6 6 0 0 1-6-6V21ZM40 2a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"></path></svg>
-        </div>
+      <div className="px-4">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40">
+          <path fill="#F06225" d="M20 0c11.046 0 20 8.954 20 20v14a6 6 0 0 1-6 6H21v-8.774c0-2.002.122-4.076 1.172-5.78a10 10 0 0 1 6.904-4.627l.383-.062a.8.8 0 0 0 0-1.514l-.383-.062a10 10 0 0 1-8.257-8.257l-.062-.383a.8.8 0 0 0-1.514 0l-.062.383a9.999 9.999 0 0 1-4.627 6.904C12.85 18.878 10.776 19 8.774 19H.024C.547 8.419 9.29 0 20 0Z" />
+          <path fill="#F06225" d="M0 21h8.774c2.002 0 4.076.122 5.78 1.172a10.02 10.02 0 0 1 3.274 3.274C18.878 27.15 19 29.224 19 31.226V40H6a6 6 0 0 1-6-6V21ZM40 2a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z" />
+        </svg>
+      </div>
       <div className="w-full flex items-center gap-x-1 h-full">
         <DropdownMenu modal={false}>
           <DropdownMenuTrigger asChild>
@@ -42,8 +55,10 @@ export const Navbar = () => {
           <Button 
             variant="ghost"
             size="icon"
-            onClick={() => {}}
-            className=""
+            onClick={() => onChangeActiveTool("select")}
+            className={cn(
+              activeTool === "select" && "bg-gray-100"
+            )}
           >
             <MousePointerClick className="size-4" />
           </Button>
